d5e6d70180d54d68af17f87360ab45450f8bb737
Tanguturisai <162011879+Tanguturisai@users.noreply.github.com>
2025-05-09 15:50:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,10 @@ st.title("Predictive Maintenance - HVAC Pump")
 st.write("Enter sensor values below:")
 
 # Example: If your model expects 5 features
-sensor_values = []
+sensor_values = [0.0] * (50- len(sensor_values))+sensor_values
+arr = np.array(sensor_values).reshape(1, -1)
+arr_scaled = scaler.transform(arr)
+prediction = model.predict(arr_scaled)
 for i in range(1, 6):  # adjust range if your model has more/less features
     val = st.number_input(f'Sensor {i}', value=0.0)
     sensor_values.append(val)
